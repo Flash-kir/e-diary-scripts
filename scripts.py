b2247456d6fa17c719e7d4970d373bc59bc5b27e
Flash-kir <flash-kir@yandex.ru>
2022-10-05 22:11:03 +0300

    Update scripts.py
    добавлены проверки нахождения нескольких учеников, случайный выбор урока и случайный выбор текста похвалы

--- scripts.py
+++ scripts.py
@@ -1,25 +1,32 @@
 import sys
+import random
 
 from datacenter.models import Lesson, Schoolkid, Chastisement, Commendation, Mark
 
 
 def get_schoolkid(schoolkid_name):
-    try:
-        return Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    except:
-        sys.exit(f'Ученик с именем {schoolkid_name} не найден')
-        
+    schoolkid = Schoolkid.objects.filter(full_name__contains=schoolkid_name)
+    if len(schoolkid) > 1:
+        sys.exit('Найдено несколько учеников с таким именем, уточните имя')
+    elif len(schoolkid) == 0:
+        sys.exit(f'Ученик с именем "{schoolkid_name}" не найден, попробуйте снова')
+    return schoolkid[0]
 
 
-def get_lessons(schoolkid, lesson_name):
-    try:
-        return Lesson.objects.get(
+def get_random_lesson(schoolkid, lesson_name):
+    lessons = Lesson.objects.filter(
             year_of_study=schoolkid.year_of_study, 
             group_letter=schoolkid.group_letter, 
             subject__title__contains=lesson_name)
-    except:
-        sys.exit(f'Урок с именем {lesson_name} не найден')
+    if len(lessons) == 0:
+        sys.exit(f'Урок с именем "{lesson_name}" не найден, попробуйте снова')
+    lesson = lessons[random.randint(0, len(lessons)-1)]
+    return lesson
         
+
+def get_random_commendation_text():
+    commendations = Commendation.objects.all()
+    return commendations[random.randint(0, len(commendations)-1)].text
 
 
 def fix_marks(schoolkid_name):
@@ -35,13 +42,16 @@ def remove_chastisements(schoolkid_name):
     chastisement.delete()
 
 
-def create_commendation(schoolkid_name, lesson_name, text='хвалю'):
+def create_commendation(schoolkid_name, lesson_name, text=''):
     schoolkid = get_schoolkid(schoolkid_name)
-    lesson = get_lessons(schoolkid, lesson_name)[0]
+    lesson = get_random_lesson(schoolkid, lesson_name)
     commendation = Commendation()
     commendation.schoolkid = schoolkid
     commendation.teacher = lesson.teacher
     commendation.subject = lesson.subject
-    commendation.text = text
+    if not text:
+        commendation.text = get_random_commendation_text()
+    else:
+        commendation.text = text
     commendation.created = lesson.date
     commendation.save()
